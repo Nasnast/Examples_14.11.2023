b3d3b2b399ea08f6834deeba078b0108bf2dfb0c
Nasnast <nasnast@ya.ru>
2023-11-23 18:04:46 +0300

    example 005 if else

---
+++ example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{ 
+    Console.Write("Ура, это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
